4ca7aac4d2948d12bca60bffeef33ad3c6e94035
Mahnoor-Adeel <113666843+Mahnoor-Adeel@users.noreply.github.com>
2025-12-26 19:53:13 +0500

    Create mongo_raw_to_parquet.py separated dag from spark code

---
+++ spark/mongo_raw_to_parquet.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_timestamp, year, month, dayofmonth, hour
+
+spark = (
+    SparkSession.builder
+    .appName("mongo-raw-archive")
+    .getOrCreate()
+)
+
+RAW_PATH = "hdfs:///bda/raw/mongo/"
+ARCHIVE_PATH = "hdfs:///bda/archive/data/"
+
+df = spark.read.json(RAW_PATH)
+
+df = df.withColumn(
+    "event_ts",
+    to_timestamp("event_timestamp")
+)
+
+(df
+ .withColumn("year", year("event_ts"))
+ .withColumn("month", month("event_ts"))
+ .withColumn("day", dayofmonth("event_ts"))
+ .withColumn("hour", hour("event_ts"))
+ .repartition("factory_id")
+ .write
+ .mode("append")
+ .partitionBy("factory_id", "year", "month", "day", "hour")
+ .parquet(ARCHIVE_PATH)
+)
+
+spark.stop()
